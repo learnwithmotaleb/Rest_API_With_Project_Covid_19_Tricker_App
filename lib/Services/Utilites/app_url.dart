f1cd4967ce737e34f402a9ff3760097a06577629
Abdul Motaleb <jmmotaleb100@gmail.com>
2023-10-05 15:45:34 +0600

    Initial commit

---
+++ lib/Services/Utilites/app_url.dart
@@ -0,0 +1,10 @@
+class AppUrl{
+
+  //==============This is base url=========
+   static final String baseUrl = "https://learnwithmotaleb.000webhostapp.com/json/covid19.json";
+
+  //=============fetch world covid states==============
+  // static const String worldStateApi = baseUrl + "all";
+  static final String countryList = "https://disease.sh/v3/covid-19/countries";
+
+}
